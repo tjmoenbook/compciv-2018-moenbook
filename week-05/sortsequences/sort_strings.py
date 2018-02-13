f00580d1390a28c1d963a784ed61aef83128606d
Tenzin Moenbook <Jangchup@DN0a1e247e.SUNet>
2018-02-12 16:22:54 -0800

    updates

--- week-05/sortsequences/sort_strings.py
+++ week-05/sortsequences/sort_strings.py
@@ -9,31 +9,16 @@ def alpha_case_insensitive():
     return sorted(STRING_LIST, key=insensitive)
 
 def by_longest_length():
-    """
-    Sort in descending order of length of strings
-    """
-    # fill it out
     def getLength(s):
         return len(s)
     return sorted(STRING_LIST, key=getLength, reverse=True)
 
-
 def filter_and_sort_number_strings():
-    """
-    Filter the original list for strings that
-    contain numbers. Then sort that list of number
-    strings but as strings (i.e. alphaebtical order)
-
-    Hint: string objects have a method named .isnumeric()
-    https://www.geeksforgeeks.org/python-string-isnumeric-application/
-    """
-    # fill it out
     tempList = []
     for s in STRING_LIST:
         if (s.isnumeric()) == True:
             tempList.append(s)
     return sorted(tempList)
-
 
 def filter_and_sort_number_strings_as_numbers():
     """
@@ -44,8 +29,5 @@ def filter_and_sort_number_strings_as_numbers():
     tempList = []
     for s in STRING_LIST:
         if (s.isnumeric()) == True:
-            tempList.append(int(s))
-    return sorted(tempList)
-
-
-
+            tempList.append(s)
+    return sorted(tempList,key=int)
